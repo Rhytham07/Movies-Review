d9eeae745446e7e4f73be64128a181c5f3868b2b
= <=>
2024-01-24 13:35:43 +0530

    Added dependednt destroy

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,9 +2,10 @@ class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_one_attached :picture
   belongs_to :user
+  has_one :cast, dependent: :destroy 
   
   validates :title, presence: true
   validates :description, presence: true
-  validates :rating,presence:true, numericality: {only_integer:true, greater_then_or_eqal_to: 5}
+  
   
 end
